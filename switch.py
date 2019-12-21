35c927082b9d050ba6eb54a5b0362453e4e008b4
Pfaffenrodt <dimitri@pfaffenrodt.de>
2019-12-21 23:49:52 +0100

    use config getter for optional properties

--- switch.py
+++ switch.py
@@ -18,8 +18,8 @@ CONF_HOST_PORT = 'port'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST_MAC_ADDRESS): cv.string,
-    vol.Required(CONF_HOST_PORT, default=9): cv.port,
-    vol.Optional(CONF_HOST_IP): cv.string,
+    vol.Required(CONF_HOST_PORT, default=DEFAULT_PORT): cv.port,
+    vol.Optional(CONF_HOST_IP, default=BROADCAST_IP): cv.string,
 })
 
 _LOGGER = logging.getLogger(__name__)
@@ -28,9 +28,9 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the device."""
 
-    MAC = config[CONF_HOST_MAC_ADDRESS]
-    IP = config[CONF_HOST_IP]
-    PORT = config[CONF_HOST_PORT]
+    MAC = config.get(CONF_HOST_MAC_ADDRESS)
+    IP = config.get(CONF_HOST_IP)
+    PORT = config.get(CONF_HOST_PORT)
     async_add_entities([Wol(MAC, IP, PORT)], True)
 
 
